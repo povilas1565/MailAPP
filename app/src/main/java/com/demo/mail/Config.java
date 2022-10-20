ee33a87f3534a1804cc6459c666640a17d196075
Paul156551 <pavel.ryzhkov.99@mail.ru>
2022-10-20 21:56:40 +0300

    Mail App

---
+++ app/src/main/java/com/demo/mail/Config.java
@@ -0,0 +1,6 @@
+package com.demo.mail;
+
+public class Config {
+    public static final String EMAIL ="ENTER YOUR EMAIL ADDRESS";
+    public static final String PASSWORD ="ENTER YOUR EMAIL ID PASSWORD";
+}
